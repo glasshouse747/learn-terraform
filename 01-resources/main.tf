ac0e3e2630881df70b845ee1a9a37137cd6c317a
Richard Okoro <glasshouse747@gmail.com>
2023-09-25 16:08:22 +0100

    Expense application commit

--- 01-resources/main.tf
+++ 01-resources/main.tf
@@ -6,3 +6,11 @@ resource "aws_instance" "web" {
     Name = "HelloWorld"
   }
 }
+
+resource "aws_route53_record" "www" {
+  zone_id = "Z04449162OZ21YMXF20XW"
+  name    = "test.mydevops75.online"
+  type    = "A"
+  ttl     = 5
+  records = [aws_instance.web.private_ip]
+}
