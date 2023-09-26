9fb1aadf517a870e90598e90a6a69fe0ee8b696b
Richard Okoro <glasshouse747@gmail.com>
2023-09-26 14:54:12 +0100

    Expense application commit

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -7,15 +7,6 @@ resource "aws_instance" "frontend" {
     Name = "frontend"
   }
 
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/expense-ansible-iteration2
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
-EOF
-
-  }
 }
 
 resource "aws_route53_record" "frontend" {
@@ -26,6 +17,19 @@ resource "aws_route53_record" "frontend" {
   records = [ aws_instance.frontend.private_ip ]
 }
 
+resource "null_resource" "frontend" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible-iteration2
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.frontend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+EOF
+
+  }
+
+}
 
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
@@ -34,16 +38,6 @@ resource "aws_instance" "mysql" {
 
   tags = {
     Name = "mysql"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/expense-ansible-iteration2
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
-EOF
-
   }
 }
 
@@ -55,6 +49,20 @@ resource "aws_route53_record" "mysql" {
   records = [ aws_instance.mysql.private_ip ]
 }
 
+resource "null_resource" "mysql" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible-iteration2
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.mysql.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+EOF
+
+  }
+
+}
+
 resource "aws_instance" "backend" {
   ami           = data.aws_ami.ami.image_id
   instance_type = "t3.micro"
@@ -62,16 +70,6 @@ resource "aws_instance" "backend" {
 
   tags = {
     Name = "backend"
-  }
-
-  provisioner "local-exec" {
-    command = <<EOF
-cd /home/centos/expense-ansible-iteration2
-git pull
-sleep 60
-ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
-EOF
-
   }
 }
 
@@ -81,4 +79,18 @@ resource "aws_route53_record" "backend" {
   type    = "A"
   ttl     = 5
   records = [ aws_instance.backend.private_ip ]
+}
+
+resource "null_resource" "backend" {
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible-iteration2
+git pull
+sleep 60
+ansible-playbook -i ${aws_instance.backend.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+EOF
+
+  }
+
 }
