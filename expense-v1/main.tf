86729019d8fd76579d9da72dc36c202e523685a9
Richard Okoro <glasshouse747@gmail.com>
2023-09-26 14:37:07 +0100

    Expense application commit

--- expense-v1/main.tf
+++ expense-v1/main.tf
@@ -6,6 +6,16 @@ resource "aws_instance" "frontend" {
   tags = {
     Name = "frontend"
   }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible-iteration2
+git pull
+sleep 60
+ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=frontend
+EOF
+
+  }
 }
 
 resource "aws_route53_record" "frontend" {
@@ -16,23 +26,6 @@ resource "aws_route53_record" "frontend" {
   records = [ aws_instance.frontend.private_ip ]
 }
 
-resource "aws_instance" "backend" {
-  ami           = data.aws_ami.ami.image_id
-  instance_type = "t3.micro"
-  vpc_security_group_ids = [data.aws_security_group.sg.id]
-
-  tags = {
-    Name = "backend"
-  }
-}
-
-resource "aws_route53_record" "backend" {
-  zone_id = data.aws_route53_zone.zone.id
-  name    = "backend.${var.zone_id}"
-  type    = "A"
-  ttl     = 5
-  records = [ aws_instance.backend.private_ip ]
-}
 
 resource "aws_instance" "mysql" {
   ami           = data.aws_ami.ami.image_id
@@ -42,6 +35,16 @@ resource "aws_instance" "mysql" {
   tags = {
     Name = "mysql"
   }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible-iteration2
+git pull
+sleep 60
+ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=mysql
+EOF
+
+  }
 }
 
 resource "aws_route53_record" "mysql" {
@@ -50,4 +53,32 @@ resource "aws_route53_record" "mysql" {
   type    = "A"
   ttl     = 5
   records = [ aws_instance.mysql.private_ip ]
+}
+
+resource "aws_instance" "backend" {
+  ami           = data.aws_ami.ami.image_id
+  instance_type = "t3.micro"
+  vpc_security_group_ids = [data.aws_security_group.sg.id]
+
+  tags = {
+    Name = "backend"
+  }
+
+  provisioner "local-exec" {
+    command = <<EOF
+cd /home/centos/expense-ansible-iteration2
+git pull
+sleep 60
+ansible-playbook -i ${self.private_ip}, -e ansible_user=centos -e ansible_password=DevOps321 main.yml -e role_name=backend
+EOF
+
+  }
+}
+
+resource "aws_route53_record" "backend" {
+  zone_id = data.aws_route53_zone.zone.id
+  name    = "backend.${var.zone_id}"
+  type    = "A"
+  ttl     = 5
+  records = [ aws_instance.backend.private_ip ]
 }
