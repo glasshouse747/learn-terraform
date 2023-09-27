dd31d70f2137d9c052447bc82ca4fb1a3ba1e3bf
Richard Okoro <glasshouse747@gmail.com>
2023-09-27 14:23:33 +0100

    Expense application commit

---
+++ expense-v2/app-create/data.tf
@@ -0,0 +1,23 @@
+data "aws_ami" "ami" {
+  most_recent      = true
+  name_regex       = "Centos-8-DevOps-Practice"
+  owners           = ["973714476881"]
+}
+
+data "aws_security_group" "sg" {
+  name = "default"
+}
+
+data "aws_route53_zone" "zone" {
+  name         = var.zone_id
+}
+
+variable "zone_id" {
+  default = "mydevops75.online"
+}
+
+locals {
+  ami = data.aws_ami.ami.image_id
+  zone_id = data.aws_route53_zone.zone.zone_id
+}
+
