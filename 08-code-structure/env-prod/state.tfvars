0f341a3e39dd96284a75594a39c6cd0bb659a58b
Richard Okoro <glasshouse747@gmail.com>
2023-09-28 14:57:09 +0100

    Expense application commit

---
+++ 08-code-structure/env-prod/state.tfvars
@@ -0,0 +1,3 @@
+bucket = "my75-devops-terraform"
+key    = "v1-root/dev/terraform.tfstate"
+region = "us-east-1"
---
+++ 08-code-structure/main.tf
@@ -0,0 +1,11 @@
+resource "null-resource" "test" {
+  provisioner "local-exec" {
+    command = "echo Hello World - ${var.env} Environment"
+   }
+}
+
+variable "env" {}
+
+terraform {
+  backend "s3" {}
+}
